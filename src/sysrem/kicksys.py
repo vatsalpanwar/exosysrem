48e265dbd9f6b6c115897aceccd18b41c756183c
Vatsal Panwar <v.panwar@uva.nl>
2021-06-22 18:07:41 +0200

    Adding some limbs to the package

--- src/sysrem/kicksys.py
+++ src/sysrem/kicksys.py
@@ -3,3 +3,35 @@ import scipy as sp
 
 
 class kicksys():
+
+    """
+    This class allows the user to take the matrix of time series spectra
+    which have been already masked out appropriately for regions of strong
+    telluric absorption and detector artefact (like gaps for example) and apply the
+    SysRem algorithm from Mazeh et al. 2007 and obtain the correction factor for systematics
+    occuring linearly in the time series spectra due to instrument, tellurics, stellar absorption, etc.
+    and ideally preserve the planet signals after an optimum number of iteration.
+
+
+    Parameters
+    ----------
+    specmat : 2D matrix of spectra with the shape as time x wavelength, necessary
+    specmat_err : 2D matrix of spectra error (e.g. those derived from optimal extraction, will
+    be used as a denominator in the expression sysrem minimizes.), necessary
+
+    """
+
+    def __init__(self, specmat = None, specmat_err = None):
+
+        self.specmat = specmat
+        self.specmat_err = specmat_err
+
+
+
+
+
+
+
+
+
+
